a90d141fcef9b3de9cabedde5998136b71cc8f8a
Andrew Baldwin <andrew.baldwin44@gmail.com>
2021-02-20 10:37:02 -0500

    Webpack asset management

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import _ from "lodash";
 
-function component() {
+import "./styles/style.css";
+import "./styles/style.scss";
+import Icon from "./assets/images/icon.jpeg";
+
+function component({ name, className }) {
   const element = document.createElement("div");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello", name], " ");
+  element.classList.add(className);
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(
+  component({ name: "webpack", className: "hello-webpack" })
+);
+document.body.appendChild(component({ name: "sass", className: "hello-sass" }));
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,36 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: { sourceMap: true }
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource"
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource"
+      }
+    ]
   }
 };
